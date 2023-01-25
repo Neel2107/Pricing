a4c08cd50e19d1aad2e84d9ceec2505c394afe5f
Neel <neelpatel805804@gmail.com>
2023-01-26 01:56:25 +0530

    inital commit

--- src/Components/Plans.jsx
+++ src/Components/Plans.jsx
@@ -3,103 +3,111 @@ import React from "react";
 export const Plans = () => {
   return (
     <div>
-      <div>
-        <h1>Pick a base plan</h1>
+      <div className="main-text">
+        <h1>Pick a base plan:</h1>
       </div>
 
-      <div>
-        <div>
-          <h1>Basic</h1>
-        </div>
+      <div className="container c-row">
+        <div className="card text-center p-3">
+          <div>
+            <h1 className="fw-bold text-secondary">Basic</h1>
+          </div>
 
-        <div>
-          <p>A no-frills plan to meet most landlords' requirements.</p>
-        </div>
+          <div>
+            <p className="fw-bold text-secondary">A no-frills plan to meet most landlords' requirements.</p>
+          </div>
 
-        <div>$12.55/mo</div>
+          <div className="text-secondary">$12.55/mo</div>
 
-        <div>
-          <p>KEY FEATURES</p>
-        </div>
+          <div>
+            <p className="text-secondary">KEY FEATURES</p>
+          </div>
 
-        <div>
-          <p>Everyday Belongings</p>
-          <p>$10,000</p>
+          <div className="fetures text-secondary">
+            <p>Everyday Belongings</p>
+            <p>$10,000</p>
+          </div>
+          <div className="fetures text-secondary">
+            <p>Portable Electronics</p>
+            <p>$1,000</p>
+          </div>
+          <div className="fetures text-secondary">
+            <p>Personal Liability</p>
+            <p>$100,000</p>
+          </div>
         </div>
-        <div>
-          <p>Portable Electronics</p>
-          <p>$1,000</p>
+      
+          
+        <div className="card text-center p-3 shadow p-3 bg-body rounded mx-2 ">
+        <span className="mid btn-primary btn-sm color-git">
+            MOST POPULAR
+          </span>
+
+          <div>
+            <h1 className="text-primary fw-bold">Essential</h1>
+          </div>
+
+          <div>
+            <p className="fw-bold">Our top-seller for"the average renter."</p>
+          </div>
+
+          <div>$18.55/mo</div>
+
+          <div>
+            <p>KEY FEATURES</p>
+          </div>
+
+          <div className="fetures">
+            <p>Everyday Belongings</p>
+            <p>$20,000</p>
+          </div>
+          <div className="fetures">
+            <p>Portable Electronics</p>
+            <p>$1,000</p>
+          </div>
+          <div className="fetures">
+            <p>Personal Liability</p>
+            <p>$200,000</p>
+          </div>
         </div>
-        <div>
-          <p>Personal Liability</p>
-          <p>$100,000</p>
+        <div className="card text-center p-3">
+          <div>
+            <h1 className="fw-bold text-secondary">Premium</h1>
+          </div>
+
+          <div>
+            <p className="fw-bold text-secondary">The perfect plan for those with a few extra belongings.</p>
+          </div>
+
+          <div className="text-secondary">$33.55/mo</div>
+
+          <div>
+            <p className="text-secondary">KEY FEATURES</p>
+          </div>
+
+          <div className="fetures text-secondary">
+            <p>Everyday Belongings</p>
+            <p>$40,000</p>
+          </div>
+          <div className="fetures text-secondary">
+            <p>Portable Electronics</p>
+            <p>$3,000</p>
+          </div>
+          <div className="fetures text-secondary">
+            <p>Personal Liability</p>
+            <p>$300,000</p>
+          </div>
         </div>
       </div>
-      <div>
+      <div className="plans">
         <div>
-            <h2>MOST POPULAR</h2>
-        </div>
-        <div>
-          <h1>Essential</h1>
-        </div>
-
-        <div>
-          <p>Our top-seller for"the average renter."
-          </p>
-        </div>
-
-        <div>$18.55/mo</div>
-
-        <div>
-          <p>KEY FEATURES</p>
-        </div>
-
-        <div>
-          <p>Everyday Belongings</p>
-          <p>$20,000</p>
-        </div>
-        <div>
-          <p>Portable Electronics</p>
-          <p>$1,000</p>
-        </div>
-        <div>
-          <p>Personal Liability</p>
-          <p>$200,000</p>
+          <button className="btn btn-primary rounded-pill px-4 mt-3">
+            Choose Plan
+          </button>
         </div>
       </div>
-      <div>
-        <div>
-          <h1>Premium</h1>
-        </div>
-
-        <div>
-          <p>The perfect plan for those with a few extra belongings.</p>
-        </div>
-
-        <div>$33.55/mo</div>
-
-        <div>
-          <p>KEY FEATURES</p>
-        </div>
-
-        <div>
-          <p>Everyday Belongings</p>
-          <p>$40,000</p>
-        </div>
-        <div>
-          <p>Portable Electronics</p>
-          <p>$3,000</p>
-        </div>
-        <div>
-          <p>Personal Liability</p>
-          <p>$300,000</p>
-        </div>
-      </div>
-      <div>
-        <button>Choose Plan</button>
-      </div>
-      <div>
-        <p>Continue to view or edit details.</p>
+      <div className="last-txt">
+        <p className="fw-lighter mt-2">Continue to view or edit details.</p>
       </div>
     </div>
   );
